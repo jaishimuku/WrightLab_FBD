1bf924fdc12834763f34e9f4c3bae95fe834db2c
Mukunda Jaishi <mukunda.jaishi@selu.edu>
2020-05-29 20:20:57 -0500

    folder managed

--- SampFBD.R
+++
@@ -1,9 +0,0 @@
-SampFBD <- function(n_extant, n_trees, df,  mu, lambda, psi, rows, mers){
-  m1 <- df[rows, mu]
-  l1 <- df[rows, lambda]
-  p1 <- df[rows, psi]
-  
-  modelledfoss <- sim.fbd.rateshift.taxa(n_extant, n_trees, l1, m1, p1, mers, complete = TRUE)
-  return(modelledfoss)
-}
-
