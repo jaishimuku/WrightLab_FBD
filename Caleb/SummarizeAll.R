6f05cd25382f54b416a0532a0216ae1097586786
W0650629 <57684423+W0650629@users.noreply.github.com>
2020-06-26 07:44:09 -0700

    Add files via upload

--- Caleb/SummarizeAll.R
+++ Caleb/SummarizeAll.R
@@ -22,14 +22,14 @@ if(by == FALSE){
 }
 
 else{
-for(i in which(c(1:nrow(data) %% by == 0))){
+  for(i in which(c(1:nrow(data) %% by == 0))){
   
-  tempTree <- SFB(n_extant = extant, n_trees = 1, df = data, mu = mus, lambda = lambdas, psi = psis, row = i, mers = intervals)
-  tree_stats <- SummPhylo(tempTree, intervals)
+    tempTree <- SFB(n_extant = extant, n_trees = 1, df = data, mu = mus, lambda = lambdas, psi = psis, row = i, mers = intervals)
+    tree_stats <- SummPhylo(tempTree, intervals)
   
-  for(i in c(1:length(tree_stats))){
-    summlist[[i]] <- append(summlist[[i]], tree_stats[i])
-  }
+    for(i in c(1:length(tree_stats))){
+      summlist[[i]] <- append(summlist[[i]], tree_stats[i])
+    }
   }
 }
 
@@ -42,7 +42,7 @@ for(i in c(1:ncol(summlist))){
   names(summlist)[2] <- "Total_Foss_Count"
   
   if(i > 2){
-    names(summlist)[i] <- paste("Int", i - 2, "Fossils" ,sep = "_")
+    names(summlist)[i] <- paste("Int", i - 2, "Fossils", sep = "_")
   }
 }
 
@@ -57,8 +57,9 @@ if(length(path) != 0){
 return(summlist)
 }
 
-\
 
 
 
 
+testcolor = "black"
+ggplot(sampedtree, aes(x= OriginTime)) + geom_histogram(bins = 40 ,color = "purple") + ggtitle("                            Estimated Origin Times") + ylab("Occurences") + xlab("Origin Time Estimates (mya)") + geom_vline(xintercept = 175, color = "red") + scale_x_continuous(c(1,xlabels)) + geom_text(aes(color = testcolor, 131, 15, label = "Predicted Origin Time:131 mya ->                                                                  ")) + scale_x_reverse() + theme(legend.position = "none")
